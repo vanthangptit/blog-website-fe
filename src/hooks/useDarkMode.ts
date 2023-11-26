c736b986b6d9f3336e6f38da952a957fa0423b40
Daniel Nguyen <daniel@dandelionlabs.io>
2023-11-26 11:37:52 +0700

    - Create userDarkMode hook
    Signed-off-by: Daniel Nguyen <daniel@dandelionlabs.io>

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { themes } from '@src/constants';
+
+export const useDarkMode = () => {
+  const [ theme, setTheme ] = useState(themes.lightMode);
+  const [ mountedComponent, setMountedComponent ] = useState(false);
+
+  const setMode = (mode: string) => {
+    window.localStorage.setItem('theme', mode);
+    setTheme(mode);
+  };
+
+  const themeToggler = () => {
+    theme === themes.lightMode ? setMode(themes.darkMode) : setMode(themes.lightMode);
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+    localTheme ? setTheme(localTheme) : setMode(themes.lightMode);
+    setMountedComponent(true);
+  }, []);
+
+  return [ theme, themeToggler, mountedComponent ];
+};
