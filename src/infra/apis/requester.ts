2046f033b526a83170c28ed0e4ecb07bdb43a3c9
Daniel Nguyen <daniel@dandelionlabs.io>
2023-12-03 15:27:34 +0700

    - Moving apis folder to infra folder
    Signed-off-by: Daniel Nguyen <daniel@dandelionlabs.io>

---
+++ src/infra/apis/requester.ts
@@ -0,0 +1,12 @@
+// import axios, { AxiosResponse } from 'axios';
+
+// const responseBody = (response: AxiosResponse) => response.data;
+
+const requester = {
+  // get: (url: string, params = {}, config = {}) => axios.get(url, { params, ...config }).then(responseBody),
+  // post: (url: string, data = {}, config = {}) => axios.post(url, data, config).then(responseBody),
+  // put: (url: string, data = {}, config = {}) => axios.put(url, data, config).then(responseBody),
+  // delete: (url: string, params = {}, config = {}) => axios.delete(url, { params, ...config }).then(responseBody)
+};
+
+export default requester;
