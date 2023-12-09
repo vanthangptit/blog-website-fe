a75508ab3dd801c81a93472aca305b05641716ba
Daniel Nguyen <daniel@dandelionlabs.io>
2023-12-09 16:37:14 +0700

    - Create uploadFile and deleteFile function
    Signed-off-by: Daniel Nguyen <daniel@dandelionlabs.io>

---
+++ src/constants/aws/s3.ts
@@ -0,0 +1,5 @@
+export const AWS_S3_REGION = process.env.REACT_APP_AWS_S3_REGION;
+export const AWS_S3_NAME = process.env.REACT_APP_AWS_S3_NAME;
+export const AWS_S3_IDENTITY_POOL_ID = process.env.REACT_APP_AWS_S3_IDENTITY_POOL_ID;
+export const AWS_S3_URL = process.env.REACT_APP_AWS_S3_URL;
+export const AWS_S3_URL_BLOG = process.env.REACT_APP_AWS_S3_URL_BLOG;
