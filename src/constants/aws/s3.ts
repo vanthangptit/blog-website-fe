8c705f3bed7f7937656e702d025d5dada5fade54
Daniel Nguyen <thang.dev.ptit@gmail.com>
2024-02-25 16:04:11 +0700

    - Update code

--- src/constants/aws/s3.ts
+++ src/constants/aws/s3.ts
@@ -1,9 +1,7 @@
 export const AWS_S3_REGION = process.env.REACT_APP_AWS_S3_REGION;
 export const AWS_S3_NAME = process.env.REACT_APP_AWS_S3_NAME;
 export const AWS_S3_IDENTITY_POOL_ID = process.env.REACT_APP_AWS_S3_IDENTITY_POOL_ID;
-export const AWS_S3_URL = process.env.REACT_APP_AWS_S3_URL;
-export const AWS_S3_URL_BLOG = process.env.REACT_APP_AWS_S3_URL_BLOG;
 
-export const AVATAR_DEFAULT = 'https://blog-app-seeder.s3.ap-southeast-1.amazonaws.com/images/avatar.svg';
+export const AVATAR_DEFAULT = 'https://app-blog-website.s3.ap-southeast-1.amazonaws.com/static/avatar.svg';
 export const TEXT_LOGO_BLACK = 'https://app-blog-website.s3.ap-southeast-1.amazonaws.com/static/logo-black.png';
 export const TEXT_LOGO_WHITE = 'https://app-blog-website.s3.ap-southeast-1.amazonaws.com/static/logo-white.png';
